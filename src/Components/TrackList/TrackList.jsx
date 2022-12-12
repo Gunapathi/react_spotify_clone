9d768ecc421e6ea426e294594a7c13ffa3493250
Gunapathi <gunapathi123@gmail.com>
2022-12-13 01:16:24 +0530

    tracklist updated

--- src/Components/TrackList/TrackList.jsx
+++ src/Components/TrackList/TrackList.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { BsFillVolumeUpFill, BsMusicNoteList } from "react-icons/bs";
 import { FaDesktop } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { RiDiscFill } from "react-icons/ri";
 
 const TrackList = () => {
 	return (
 		<div className="trackList hidden sm:block mb-4">
+			<div className="top text-primary flex items-center mb-4">
+				<RiDiscFill className="text-primaryTextWhite text-[52px] mr-2" />
+				<p className="text-primaryTextWhite text-[16px]">
+					Sample Name
+					<span className="block text-primary text-[14px]">
+						Artist Name
+					</span>
+				</p>
+			</div>
 			<div className="bottom flex items-center justify-between text-primary text-[18px]">
 				<motion.i
 					whileTap={{ scale: 0.75 }}
